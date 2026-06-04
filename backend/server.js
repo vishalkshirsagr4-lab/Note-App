ac7d7a6c6a081e63e990be638bb142aeeccf8f69
laxmi <laxmidodahatti@gmail.comm>
2026-06-04 15:16:20 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:' http://localhost:5173/',
+    credentials:true
+}))
 
 
 app.use('/api/auth',auth);
