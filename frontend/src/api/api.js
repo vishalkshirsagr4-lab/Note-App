a5947de41057dfc2464deadc61ae74b00d2d3e9e
laxmi <laxmidodahatti@gmail.comm>
2026-06-04 15:02:56 +0530

    first commit

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api' ,
+  baseURL: 'https://note-app-z9su.onrender.com/api' ,
 });
 
 api.interceptors.request.use((config) => {
